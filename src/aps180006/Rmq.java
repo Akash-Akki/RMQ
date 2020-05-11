5e75b39f68e6e086b15e61712432f6a412c08bd1
akash-akki <akashpakki@gmail.com>
2020-05-10 22:51:18 -0500

    complete implementation of rmq

--- src/aps180006/Rmq.java
+++ src/aps180006/Rmq.java
@@ -1,96 +1,107 @@
+/**
+ * @author  Akash P Akki      netid: apa190001
+ * @author  Anant Srivastava  netid: aps180006
+ */
 package aps180006;
 
-import java.sql.SQLSyntaxErrorException;
+
 
 public class Rmq {
 
-  public int findRmq(int[] a, int i ,int j) {
+  Timer timer = new Timer();
+
+  /**
+   * This method takes a input and constructs a sparse tabel for the block minima array of given input array a
+   * @param a input array a
+   * @return sparse table
+   */
+  public int[][] preProcessing(int[] a) {
     double b = Math.log(a.length)/Math.log(2);
     int blockSize= (int) Math.ceil(b);
-
-   // System.out.println("block size "+blockSize);
-    int min1 = Integer.MAX_VALUE;
-    int min2 = Integer.MAX_VALUE;
-    int min3 = Integer.MAX_VALUE;
-    int min =Integer.MAX_VALUE;
     int[] blockMinima = constructBlockMinima(a,blockSize);
     int colSize = findSizeOfSparseTable(blockMinima.length);
-    //System.out.println(colSize);
-//    for(int k=0;k<blockMinima.length;k++){
-//      System.out.print(blockMinima[k]+ " ");
-//    }
-    //System.out.println("  ");
-   int[][] sparse = constructSparseTable(blockMinima,colSize);
+    int[][] sparse = constructSparseTable(blockMinima,colSize);
+    return  sparse;
+  }
 
-//   for(int k=0;k<blockMinima.length;k++){
-//     for(int l=0;l<colSize;l++){
-//       System.out.print(sparse[k][l]+" ");
-//     }
-//     System.out.println();
-//   }
-
+  /**
+   *  This method finds the minimum element between the two indices i and j by looking up sparse table
+   * @param a given input array a
+   * @param i given input index i
+   * @param j given input index j
+   * @param sparse sparse table of block minima array
+   * @return minmum element between the given two indices i and j
+   */
+  public int findRmq(int[] a, int i ,int j,int[][] sparse) {
+    int arrayLength = a.length;
+    double b = Math.log(arrayLength)/Math.log(2);
+    int blockSize= (int) Math.ceil(b);
+    int min1 = Integer.MAX_VALUE;
+    int min3 = Integer.MAX_VALUE;
+    int min =0;
    if( i/blockSize == j/blockSize){
-    // System.out.println("here 9");
      min=traverseIBlock(i,a, blockSize, j);
    }
-   else if(i % blockSize!=0 && (j % blockSize != blockSize-1 && j!=a.length)){
-     //System.out.println("here 1");
-        min1 = traverseIBlock(i,a,blockSize,j);
-        min2 = traverseJBlock(j - j % blockSize,a,j);
-      //  System.out.println("min1 "+min1);
-        //System.out.println("min 2 "+min2);
-      //System.out.println((j/blockSize) -1);
+   else if(i % blockSize!=0 && (j % blockSize != blockSize-1 && j!=arrayLength)){
+        min1 = traverseJBlock(j - j % blockSize,a,j);
+        Math.min(traverseIBlock(i,a,blockSize,j), traverseJBlock(j - j % blockSize,a,j));
      if((j/blockSize - i/blockSize ) >1 )
-        min3 = query(sparse,(i/blockSize)+1,(j/blockSize) -1);
-    // System.out.println("min 3 "+min3);
-        min = Math.min(min3 ,Math.min(min1,min2));
+        min3 = query(sparse,(i/blockSize)+1,Math.max((j/blockSize -1),0));
+     min = Math.min(min3 ,min1);
 
     }
-    else if(i % blockSize !=0 && (j%blockSize == blockSize-1 || j==a.length)){
-     // System.out.println("here 2");
+    else if(i % blockSize !=0 && (j%blockSize == blockSize-1 || j==arrayLength)){
       min1 = traverseIBlock(i,a,blockSize,j);
       min3 = query(sparse,i/blockSize+1 ,j/blockSize);
       min= Math.min(min1,min3);
     }
-    else if( i % blockSize == 0 && (j % blockSize != blockSize-1 && j!=a.length)){
-      //System.out.println("here 3");
-      min2 = traverseJBlock(j - j%blockSize,a,j);
-      min3 = query(sparse,(i/blockSize),Math.max(j/blockSize -1,0));
-      min= Math.min(min2,min3);
+    else if( i % blockSize == 0 && (j % blockSize != blockSize-1 && j!=arrayLength)){
+      min1 = traverseJBlock(j - j%blockSize,a,j);
+       min3 = query(sparse,(i/blockSize),Math.max((j/blockSize -1),0));
+      min= Math.min(min1,min3);
     }
     else {
-      //System.out.println("here 4");
        min = query(sparse, i / blockSize, j / blockSize);
     }
-    System.out.println("  "+min);
     return min;
   }
 
   private int traverseJBlock(int l, int[] a, int j) {
     int min =Integer.MAX_VALUE;
-     //System.out.println("j");
     for(l=l;l<=j;l++){
       min = Math.min( min,a[l]);
     }
     return min;
   }
 
+  /**
+   * This method traverses the array from initial index i and return the minimum within that block
+   * @param i given index i
+   * @param a input array a
+   * @param blockSize the block size of given array a
+   * @param j given index j
+   * @return
+   */
   private int traverseIBlock(int i, int[] a ,int blockSize,int j) {
     int min = Integer.MAX_VALUE;
      int blockNumber = (i/blockSize)+1;
-    //System.out.println("b " +blockNumber*blockSize);
-    //System.out.println("i is "+i);
-    for(i=i ;i<blockNumber*blockSize && i<= j ; i++){
+     int len = blockNumber*blockSize;
+    for(i=i ;i<len && i<= j ; i++){
       min = Math.min(a[i],min);
 
     }
     return min;
   }
 
+  /**
+   * This method is used to query the sparse table between the given two indices and return the minimum
+   * @param sparse constructed sparse table of block minima array of given array a
+   * @param i given index i
+   * @param j given index j
+   * @return returns the minimum using the given block minima
+   */
   private int query(int[][] sparse, int i, int j) {
       int k = findLargestK(i,j);
-     // System.out.println("j is "+j);
-      //System.out.println("k is "+k);
       int[][] ranges = {{i, (int) (i+Math.pow(2,k)-1)},{(int) (j - (Math.pow(2,k))+1),j}};
       int min=findMin(sparse,ranges);
       return min;
@@ -98,6 +109,12 @@ public class Rmq {
 
   }
 
+  /**
+   * This method makes the constant lookup of the given two ranges and returns the minimum element
+   * @param sparse constructed sparse table of block minima
+   * @param ranges given ranges of the sparse table
+   * @return return the minimum using the sparse table
+   */
   private int findMin(int[][] sparse, int[][] ranges) {
     int min =Integer.MAX_VALUE;
     int i1 = ranges[0][0];
@@ -106,19 +123,33 @@ public class Rmq {
     int j1 = (int) (Math.log(diffRange1)/Math.log(2));
     int diffRange2 = ranges[1][1] - i2 + 1;
     int j2 = (int)Math.log((diffRange2)/Math.log(2));
-    int val1 = sparse[i1][j1];
-    int val2 = sparse[i2][j2];
-    return Math.min(val1,val2);
+    return Math.min(sparse[i1][j1],sparse[i2][j2]);
   }
 
+  /**
+   * This method calculates k such the 2^k <= j-i+1
+   * @param i given index i
+   * @param j given index j
+   * @return return k
+   */
   private int findLargestK(int i, int j) {
     int k=0;
-     while(Math.pow(2,k)<j-i+1){
+     while(Math.pow(2,k) <= j-i+1){
        k++;
+       if(Math.pow(2,k) > j-i+1){
+         k--;
+         break;
+       }
      }
     return k;
   }
 
+  /**
+   * This method constructs the block minima array using input array a and blocksize
+   * @param a given array a
+   * @param blockSize block size of the given array a
+   * @return return the block minima array
+   */
   private int[] constructBlockMinima(int[] a, int blockSize) {
     double blockMinimaSize = (double)a.length/blockSize;
     int[] blockMinima = new int[(int) Math.ceil(blockMinimaSize)];
@@ -135,15 +166,21 @@ public class Rmq {
     return blockMinima;
   }
 
+  /**
+   * This method constructs the sparse table  of the block minima array
+   * @param blockMinima constructed block minima array
+   * @param colsize column size of the sparsetable
+   * @return return sparse table of given block minima array
+   */
   private int[][] constructSparseTable(int[] blockMinima,int colsize) {
       int rowSize = blockMinima.length;
-      int[][] sparseTable = new int[blockMinima.length][colsize];
-      for(int i=0;i<blockMinima.length;i++){
+      int[][] sparseTable = new int[rowSize][colsize];
+      for(int i=0;i<rowSize;i++){
         sparseTable[i][0]=blockMinima[i];
       }
       for(int j=1 ; j < colsize;j++){
-        for(int i=0; i < blockMinima.length ; i++ ){
-          if(  (i + Math.pow(2,j-1)) >= blockMinima.length)
+        for(int i=0; i < rowSize ; i++ ){
+          if(  (i + Math.pow(2,j-1)) >= rowSize)
               break;
           sparseTable[i][j] = Math.min(sparseTable[i][j-1], sparseTable[(int) (i + Math.pow(2,j-1))][j-1]);
         }
@@ -151,6 +188,11 @@ public class Rmq {
      return sparseTable;
   }
 
+  /**
+   * This method calculates the size of the column of the sparse table to be constructed.
+   * @param length length of the constructed block Minima
+   * @return return columnsize of the sparse table to be constructed
+   */
   private int findSizeOfSparseTable(int length) {
      int colsize=1;
      int i=0;
@@ -164,6 +206,7 @@ public class Rmq {
      }
     return colsize;
   }
+
 
 
 }
--- src/aps180006/RmqDriver.java
+++ src/aps180006/RmqDriver.java
@@ -1,31 +1,39 @@
+/**
+ * @author  Akash P Akki      netid: apa190001
+ * @author  Anant Srivastava  netid: aps180006
+ */
+
+
 package aps180006;
 
+import java.util.Random;
 
 public class RmqDriver {
 
-
-
-
-
   public static  void  main(String[] args){
-
     Rmq rmq = new Rmq();
-    int[] arr = new int[]{4,5,8,7,9,4,5,8,4,8,7};
-
-   // System.out.println("arr"+arr.length);
-    for(int i= 0; i< arr.length;i++){
-      for(int j=i;j<arr.length;j++){
-        System.out.print(" "+i+ " "+j);
-        rmq.findRmq(arr,i,j);
-      }
+    Random 	random = new Random();
+    int n=100;
+    int i=0;
+    int j=n-1;
+    if(args.length > 0) {
+      n = Integer.parseInt(args[0]);
+      i = Integer.parseInt(args[1]);
+      j =Integer.parseInt(args[2]);
     }
-
-
-
-
-
-
-
+   int[] arr = new int[n];
+    for(int k=0; k<n; k++) {
+      arr[k] = random.nextInt();
+    }
+    Timer timer = new Timer();
+    timer.start();
+    System.out.println(" start");
+    timer.start();
+    int[][] sparse = rmq.preProcessing(arr);
+    System.out.println(timer.end());
+    timer.start();
+    int min = rmq.findRmq(arr,i,j,sparse);
+   System.out.println(timer.end());
   }
 
 }
